d5f3c00338b5fc4fce23ccabab276aa4343c4582
Charlie Fish <fishcharlie@me.com>
2017-09-24 16:00:35 -0600

    Fixing bugs, and reogranizing code

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 const opn = require('opn');
-const pjson = require('./package.json');
-let keyword = process.argv[1];
-
-if (keyword == "open") {
-	keyword = process.argv[2];
+const path = require('path');
+try {
+	require(path.join(process.cwd(), 'package.json'));
+} catch (e) {
+	console.error("Can't find package.json file.");
+	process.exit(1);
 }
+const pjson = require(path.join(process.cwd(), 'package.json'));
+const argvs = process.argv;
+const parsedParams = allElementsAfter("hubbot", argvs); // ex. ['hubbot', 'bugs']
+const keyword = parsedParams[1]; // ex. 'bugs'
+
+console.log(argvs);
+console.log(parsedParams);
+console.log(keyword);
 
 
 if (keyword == "bugs" || keyword == "issues") {
@@ -16,4 +25,21 @@ if (keyword == "bugs" || keyword == "issues") {
 }
 
 // Get current directory where this was run
-// console.log(process.cwd());
+console.log(process.cwd());
+
+
+function allElementsAfter(keyword, array) {
+	let pastValue = false;
+	let newArray = [];
+	array.forEach(function (element) {
+		if (element == keyword) {
+			pastValue = true;
+		}
+		if (pastValue) {
+			newArray.push(element);
+		}
+	});
+	return newArray;
+}
+
+process.exit();
