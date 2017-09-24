a8da38d6aa99fdfa188eb2429f81fef1372bd7d2
Charlie Fish <fishcharlie@me.com>
2017-09-24 15:36:58 -0600

    Trying to fix error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const opn = require('opn');
-const pjson = require('./package.json');
-let keyword = process.argv[1];
+var opn = require('opn');
+var pjson = require('./package.json');
+var keyword = process.argv[1];
 
 if (keyword == "open") {
 	keyword = process.argv[2];
