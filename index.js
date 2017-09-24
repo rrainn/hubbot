db7326882bb5cae3374c991c69e089bfc002ddfe
Charlie Fish <fishcharlie@me.com>
2017-09-24 15:11:48 -0600

    Basic logic

---
+++ index.js
@@ -0,0 +1,17 @@
+const opn = require('opn');
+const pjson = require('./package.json');
+let keyword = process.argv[1];
+
+if (keyword == "open") {
+	keyword = process.argv[2];
+}
+
+
+if (keyword == "bugs" || keyword == "issues") {
+	opn(pjson.bugs.url);
+} else if (keywords == "home" || keywords == "homepage") {
+	opn(pjson.homepage);
+}
+
+// Get current directory where this was run
+// console.log(process.cwd());
